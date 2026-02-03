680d1a1ed171320c95f67c73125040c5d16a6670
sofmarquez <smarquezcordero@augusta.edu>
2026-02-03 12:29:48 -0500

    alg_1

--- grid_100.py
+++ grid_100.py
@@ -37,52 +37,66 @@ print(S)
 
 
 class Test_Experiment:
-    def __init__(self, positions, infected_set, num_tests, test_size, label="", seed=None):
+    def __init__(self, positions, infected_set, test_size, label="", seed=None):
                self.positions = positions
                self.infected_set = infected_set
-               self.num_tests = num_tests
                self.test_size = test_size
                self.label = label
                self.seed = seed
 
                self.test = []
                self.results = []
+               self.history = []
 
                self.negative_points = set()
                self.positive_points = set()
                self.positive_counter = Counter()
                self.repeated_positive_points = set()
                self.candidates = set()
-    
-    def generate_tests(self):
-            self.tests = []
-            if self.seed is not None:
-                np.random.seed(self.seed)
-            for x in range(self.num_tests):
-                    indices = np.random.choice(len(self.positions), self.test_size, replace = False)
-                    test = [self.positions[i] for i in indices]
-                    self.tests.append(test)
-
 
     def clasify_tests(self, test):
             return "positive" if any (item in self.infected_set for item in test) else "negative"
     
-    def run(self):
-            self.generate_tests()
-            self.results = [self.clasify_tests(test) for test in self.tests]
+    def run(self, max_tests = 10000):
+            if self.seed is not None:
+                  np.random.seed(self.seed)
 
-            for test, result in zip(self.tests, self.results):
-                    if result == "negative":
-                            self.negative_points.update(test)
-                    else:
-                            self.positive_points.update(test)
-                            self.positive_counter.update(test)
+            test_count = 0
             
-            self.repeated_positive_points = {
-                    item for item, count in self.positive_counter.items() if count >=2
-            }
+            for t in range(1, max_tests + 1):
+                
+                test_count += 1
 
-            self.candidates = (self.positive_points) - self.negative_points 
+                indices = np.random.choice(len(self.positions), self.test_size, replace= False)
+                test= [self.positions[i] for i in indices]
+                self.test.append(test)
+
+                result = self.clasify_tests(test)
+                self.results.append(result)
+
+                if result == "negative":
+                    self.negative_points.update(test)
+                else:
+                    self.positive_points.update(test)
+                    self.positive_counter.update(test)
+
+                self.candidates = (self.positive_points) - self.negative_points 
+
+                false_positives = self.candidates - self.infected_set
+                false_negatives = self.infected_set - self.candidates
+
+                self.history.append({
+                    "tests so far" : test_count,
+                    "false positives" : len(false_positives),
+                    "false negatives" : len(false_negatives)
+                })
+
+                if len(false_positives) == 0 and len(false_negatives) == 0:
+                     print("Both false positive and negative are 0")
+                     break
+                if test_count >= max_tests:
+                     print("Max tests are reached")
+                     break
 
     def compare_candidates(self):
             return self.infected_set.intersection(self.candidates)
@@ -90,58 +104,62 @@ class Test_Experiment:
     def summary(self):
             print(f"\nSummary: {self.label}")
             print("-" * 40)
-            print(f"Number of tests      : {self.num_tests}")
+            print(f"Number of tests      : {len(self.test)}")
             print(f"Test size            : {self.test_size}")
             print(f"Number of candidates : {len(self.candidates)}")
             print(f"Candidates           : {self.candidates}")
             print(f"True positives       : {len(self.compare_candidates())}")
             print(f"TP set               : {self.compare_candidates()}")
+            print(f"Final FP             : {self.history[-1]['false positives']}")
+            print(f"Final FN             : {self.history[-1]['false negatives']}")
+
+    def print_progress(self, step=500):
+        print("\nProgress snapshot:")
+        print("Tests | FP | FN")
+        print("-" * 20)
+        for h in self.history[::step]:
+            print(f"{h['tests so far']:5d} | {h['false positives']:2d} | {h['false negatives']:2d}")
+
     
-    def to_dataframe(self):
-        rows = []
-        for i, (test, result) in enumerate(zip(self.tests, self.results), start=1):
-            row = {"Test": i}
-
-            for j, item in enumerate(test, start=1):
-                row[f"Item {j}"] = item
-
-            row["Result"] = result 
-            rows.append(row)
-
-        return pd.DataFrame(rows)
+    #def to_dataframe(self):
+    #    rows = []
+    #    for i, (test, result) in enumerate(zip(self.tests, self.results), start=1):
+     #       row = {"Test": i}
+#
+ #           for j, item in enumerate(test, start=1):
+  #              row[f"Item {j}"] = item
+#
+ #           row["Result"] = result 
+  #          rows.append(row)
+#
+ #       return pd.DataFrame(rows)
     
 
 exp_20x25 = Test_Experiment(
     positions=positions,
     infected_set=S,
-    num_tests=20,
     test_size=25,
-    label="20 test with 25 items",
-    seed=1
-)
-exp_20x29 = Test_Experiment(
-    positions=positions,
-    infected_set=S,
-    num_tests=64,
-    test_size=15,
-    label="20 Test with 29 items",
+    label="Test size of 25",
     seed=1
 )
 
 
 
 exp_20x25.run()
-exp_20x29.run()
+exp_20x25.print_progress()
 
 
-df_20x25 = exp_20x25.to_dataframe()
-df_20x29 = exp_20x29.to_dataframe()
 
-with pd.ExcelWriter("100grid_results.xlsx", engine="xlsxwriter") as writer:
-    df_20x25.to_excel(writer, sheet_name="20x25", index=False)
-    df_20x29.to_excel(writer, sheet_name="20x29", index=False)
+#df_20x25 = exp_20x25.to_dataframe()
+#df_20x29 = exp_20x29.to_dataframe()
 
- 
+#with pd.ExcelWriter("100grid_results.xlsx", engine="xlsxwriter") as writer:
+ #   df_20x25.to_excel(writer, sheet_name="20x25", index=False)
+  #  df_20x29.to_excel(writer, sheet_name="20x29", index=False)
 
+for h in exp_20x25.history[::500]:
+    print(h)
+
+
+print(exp_20x25.print_progress())
 print(exp_20x25.summary())
-print(exp_20x29.summary())
